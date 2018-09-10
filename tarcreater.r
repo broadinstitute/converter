0f6539056f031fbe8aaa5d578abdf2621eda647c
Ramani Kothadia <rbk301@nyu.edu>
2018-09-10 16:35:13 -0400

    Copy of PY in R

--- tarcreater.r
+++ tarcreater.r
@@ -1,4 +1,8 @@
 #!/usr/bin/env Rscript
+
+if(!require("pacman")) install.packages("pacman")
+pacman::p_load_gh("cmap/cmapR")
+
 args <- list()
 
 set_arguments <- function() {
@@ -9,7 +13,7 @@ set_arguments <- function() {
   while (index <= sysalen && sysargs[index] != '-m'){
     option <- sysargs[index]
     if (option %in% valid){
-      args[[option]] <- sysargs[index+1]
+      args[[option]] <<- sysargs[index+1]
       index <- index + 2
     } else if (!(option %in% valid)){
       stop("# Error: Invalid Option.\n", call. = TRUE)
@@ -17,16 +21,34 @@ set_arguments <- function() {
   }
   index <- index + 1
   while (index <= sysalen){
-    args[["-m"]] <- c(args[["-m"]], sysargs[index])
+    args[["-m"]] <<- c(args[["-m"]], sysargs[index])
     index <- index + 1
   }
-  if (!("-p" %in% valid) || !("o" %in% valid)){
+  if (!("-p" %in% names(args)) || !("-o" %in% names(args))){
     stop("# Error: -p/-o required.\n", call. = TRUE)
   }
 }
 
+extract_expt_desn <- function(){
+  return(read.csv(args[["-e"]], header = TRUE, sep = ','))
+}
+
 check_gct_against_expt <- function(myfile){
-  
+  out <- parse.gctx(myfile)
+  if (out@version == "#1.2"){
+    if (!("-e" %in% args)){
+      stop("# Error: -e option required for .GCT (1.2).\n", call. = TRUE)
+    }
+  }
+  gctcid <- out@cid
+  expcsv <- extract_expt_desn()
+  if (nrow(expcsv) < length(gctcid)){
+    stop("# Error: Samles in .GCT not found in EXPT DESN file.\n", call. = TRUE)
+  } else if (nrow(expcsv) > length(gctcid)){
+    print(paste0("# Warning: Some Samples Missing in ", myfile, " file."))
+  } else {
+    print("# Success: Check OK.\n")
+  }
 }
 
 stub <- function(myfile){
@@ -38,5 +60,30 @@ check_format <- function(myfile){
   opt <- list("gct"=check_gct_against_expt, "cct"=stub)
   opt[[ext]](myfile)
 }
+
+create_tar <- function(){
+  option <- strsplit(args[["-o"]], "[/]")[[1]]
+  mydirs <- head(option[-1], -1)
+  anadir <- ""
+  for (dir in mydirs){
+    anadir <- file.path(anadir, dir)
+  }
+  tardir <- file.path(anadir, strsplit(tail(option, 1), "[.]")[[1]][1])
+  system(paste0("mkdir -p ", tardir))
+  system(paste0("mkdir -p ", tardir, "/data"))
+  system(paste0("mkdir -p ", tardir, "/parsed-data"))
+  system(paste0("cp ", args[["-e"]], " ", tardir, "/data/."))
+  system(paste0("cp ", args[["-p"]], " ", tardir, "/parsed-data/."))
+  if ("-m" %in% names(args)){
+    for (extra in args[["-m"]]){
+      system(paste0("cp ", extra, " ", tardir, "/parsed-data/."))
+    }
+  }
+  system(paste0("tar -cvf ", args[["-o"]], " ", tardir))
+  system(paste0("rm -rf ", tardir))
+}
+
 set_arguments()
+check_format(args[["-p"]])
+create_tar()
 
